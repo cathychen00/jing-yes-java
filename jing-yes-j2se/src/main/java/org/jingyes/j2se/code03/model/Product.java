04f089dc260253a9e5456cf7e5b15a8743f9ac71
chenjing <cathychen666@qq.com>
2023-08-28 19:58:57 +0800

    原来你是这样的Java03代码样例，关于继承、多态

---
+++ jing-yes-j2se/src/main/java/org/jingyes/j2se/code03/model/Product.java
@@ -0,0 +1,17 @@
+package org.jingyes.j2se.code03.model;
+
+/**
+ * @author 陈敬
+ * @date 18/1/17
+ */
+public class Product {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("[Product]getDescription()");
+    }
+}
