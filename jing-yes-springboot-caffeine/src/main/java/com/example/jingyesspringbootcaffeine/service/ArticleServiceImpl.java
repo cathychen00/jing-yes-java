607cf409e6637784cb0bccb1af37ff047e77dbd6
chenjing <cathychen666@qq.com>
2023-09-05 19:48:41 +0800

    test(ArticleServiceImpl.cs): 测试commit注释
    测试一下commit注释提交的效果
    
    BREAKING CHANGE: 改动不大，只是添加了个注释，测试一下

--- jing-yes-springboot-caffeine/src/main/java/com/example/jingyesspringbootcaffeine/service/ArticleServiceImpl.java
+++ jing-yes-springboot-caffeine/src/main/java/com/example/jingyesspringbootcaffeine/service/ArticleServiceImpl.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Cache
+ */
 @Service
 @CacheConfig(cacheNames = "ArticleServiceImpl")
 public class ArticleServiceImpl implements ArticleService {
