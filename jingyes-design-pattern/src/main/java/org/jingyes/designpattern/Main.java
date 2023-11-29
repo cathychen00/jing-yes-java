e5847dd4354ffae9130dcecdfcc6452bbffde0a5
chenjing <cathychen666@qq.com>
2023-11-29 15:59:14 +0800

    新增jingyes-design-pattern模块

---
+++ jingyes-design-pattern/src/main/java/org/jingyes/designpattern/Main.java
@@ -0,0 +1,12 @@
+package org.jingyes.designpattern;
+
+
+/**
+ * @author 陈敬
+ * @date 2023/11/29
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
