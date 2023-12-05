746516e25502c3f35196246be02260768079833f
chenjing <cathychen666@qq.com>
2023-12-05 14:57:37 +0800

    桥接模式的消息示例

---
+++ jingyes-design-pattern/src/main/java/org/jingyes/designpattern/bridge/message/MessageSender.java
@@ -0,0 +1,9 @@
+package org.jingyes.designpattern.bridge.message;
+
+/**
+ * @author 陈敬
+ * @date 2023/12/5
+ */
+public interface MessageSender {
+    void send(String msg);
+}
---
+++ jingyes-design-pattern/src/main/java/org/jingyes/designpattern/bridge/message/UrgencyMessage.java
@@ -0,0 +1,16 @@
+package org.jingyes.designpattern.bridge.message;
+
+/**
+ * @author 陈敬
+ * @date 2023/12/5
+ */
+public class UrgencyMessage extends AbstractMessage {
+    public UrgencyMessage(MessageSender sender) {
+        super(sender);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send("[紧急]" + message);
+    }
+}
