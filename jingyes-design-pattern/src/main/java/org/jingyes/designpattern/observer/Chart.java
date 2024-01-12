368f8cc9abb2ceb1907fddc6da881510a9d5de59
chenjing <cathychen666@qq.com>
2024-01-12 19:10:55 +0800

    设计模式observer

---
+++ jingyes-design-pattern/src/main/java/org/jingyes/designpattern/observer/Chart.java
@@ -0,0 +1,12 @@
+package org.jingyes.designpattern.observer;
+
+/**
+ * @author 陈敬
+ * @date 2024/1/10
+ */
+public class Chart implements Observer{
+    @Override
+    public void update(WeatherType weatherType) {
+        System.out.println("温度图表显示"+weatherType.name()+" 温度"+weatherType.getTemperature());
+    }
+}
---
+++ jingyes-design-pattern/src/main/java/org/jingyes/designpattern/observer/Observer.java
@@ -0,0 +1,9 @@
+package org.jingyes.designpattern.observer;
+
+/**
+ * @author 陈敬
+ * @date 2024/1/10
+ */
+public interface Observer {
+    void update(WeatherType weatherType);
+}
