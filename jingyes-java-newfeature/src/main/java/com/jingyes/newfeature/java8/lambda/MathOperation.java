64176d0db1da7120fcf0c9cfdcc7f015a872f495
chenjing <cathychen666@qq.com>
2024-02-19 15:40:20 +0800

    java8函数式接口

--- jingyes-java-newfeature/src/main/java/com/jingyes/newfeature/java8/lambda/MathOperation.java
+++ jingyes-java-newfeature/src/main/java/com/jingyes/newfeature/java8/lambda/MathOperation.java
@@ -4,6 +4,7 @@ package com.jingyes.newfeature.java8.lambda;
  * @author 陈敬
  * @date 2024/1/3
  */
+@FunctionalInterface
 public interface MathOperation {
     int operate(int a,int b);
 }
---
+++ jingyes-java-newfeature/src/main/java/com/jingyes/newfeature/java8/lambda/PredictDemo.java
@@ -0,0 +1,35 @@
+package com.jingyes.newfeature.java8.lambda;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * 测试java8内置的函数式接口 Predict
+ *
+ * @author 陈敬
+ * @date 2024/2/19
+ */
+public class PredictDemo {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 5, 6, 7, 8, 10);
+
+        Predicate<Integer> p1 = x -> x % 2 == 0;
+        Predicate<Integer> p2 = x -> x % 3 == 0;
+
+        print(list, p1, System.out::println);
+        print(list, p2, System.out::println);
+
+        //and连接
+        print(list, p1.and(p2), System.out::println);
+    }
+
+    private static void print(List<Integer> list, Predicate<Integer> predicate, Consumer<Integer> consumer) {
+        list.forEach(x -> {
+            if (predicate.test(x)) {
+                consumer.accept(x);
+            }
+        });
+        System.out.println("*************");
+    }
+}
