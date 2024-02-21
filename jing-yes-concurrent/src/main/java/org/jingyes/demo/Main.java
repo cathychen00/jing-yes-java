ddaa00056df63bfb8e7997610d762c9480cbf9da
chenjing <cathychen666@qq.com>
2024-02-21 16:32:55 +0800

    package

---
+++ jing-yes-concurrent/src/main/java/org/jingyes/demo/Main.java
@@ -0,0 +1,12 @@
+package org.jingyes.demo;
+
+
+/**
+ * @author 陈敬
+ * @date 2024/2/21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
